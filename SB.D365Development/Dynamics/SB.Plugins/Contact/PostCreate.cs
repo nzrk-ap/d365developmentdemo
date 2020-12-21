aeb15a15be752f043595cc94b9780d4cdd81768b
Anton Nazarenko <annaz@smart-it.com>
2020-12-21 15:12:38 +0200

    11183: Настройка проекта плагинов

---
+++ SB.D365Development/Dynamics/SB.Plugins/Contact/PostCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace SB.Plugins.Messages
+{
+    public class PostCreate : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = factory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                var target = (Entity)context.InputParameters["Target"];
+
+                //TODO: Do stuff
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
